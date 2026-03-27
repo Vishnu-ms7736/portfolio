0dc3b39a3229952e9a291f28db998f4d25627724
Vishnu-ms7736 <vv942140@gmail.com>
2026-03-27 12:38:56 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.getElementById("start-btn").addEventListener("click", function(){
+
+alert("Welcome to YourBrand 🚀");
+
+document.getElementById("hero-title").innerText =
+"Let's Build Something Amazing";
+
+});
